0eb50316be0b91b22a0ad1fafca396d43dee6d3c
mostafizur610 <mostafizurrahman273@gmail.com>
2022-10-27 18:49:52 +0600

    set category data and run in server

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./data/categories.json')
+
+app.get('/', (req, res) => {
+    res.send('Code source is running')
+})
+
+app.get('/categories', (req, res) => {
+    res.send(categories);
+})
+
+app.listen(port, () => {
+    console.log(`Code Source Pathshala running on port, ${port}`);
+})
