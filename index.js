459292861d6d2b750a3ad703bc1364696ac53405
mostafizur610 <mostafizurrahman273@gmail.com>
2022-10-27 19:33:55 +0600

    set course and category server and response

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require('./data/categories.json')
+const categories = require('./data/categories.json');
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
     res.send('Code source is running')
@@ -14,6 +14,16 @@ app.get('/', (req, res) => {
 
 app.get('/categories', (req, res) => {
     res.send(categories);
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+
+app.get('/courses/":id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourses = courses.find(course => course._id === id);
+    res.send(selectedCourses);
 })
 
 app.listen(port, () => {
